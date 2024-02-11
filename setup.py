bb3b46d21dfd219835a77bb3ae6994e468b74c1f
Jeremy Maslanko <maslankoj@gmail.com>
2024-02-11 14:34:37 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='kmtm',
     version='0.1.0',
     py_modules=['kmtm'],
-    packages=find_packages(),
-    include_package_data=True,
     author='Jeremy Maslanko',
+    author_email='maslankoj@gmail.com',
+    url='https://github.com/jmaslanko/kmtm',
     description='Simple tool to convert kilometers/miles.',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        ],
     install_requires=[
         'Click',
     ],
